408f296268cf887bf1f5df012e0b577902bf40ce
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-02-18 20:47:00 -0800

    implement a working server with database connection and succesfully get request

--- app/models/tour.js
+++ app/models/tour.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var tourSchema = new Schema({
+  name: String,
   location: String
 });
 
--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-db.js
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var mongoose = require("mongoose");
+var db = require('./config/db.js');
+require('./app/routes')(app);
+
+var Tour = require('./app/models/tour.js');
+
+var port = process.env.PORT || 8080;
+
+
+// app.use(express.bodyParser());
+app.use(express.static(__dirname + '/public'));
+
+var tour1 = Tour({
+  name: "Golden Gate Tour",
+  location: "San Francisco"
+});
+
+tour1.save(function(err){
+  if (err) throw err;
+  console.log("tour1 is saved")
+});
+
+
+app.listen(port);
+
+console.log("You are connected to the port ", port);
+
+exports = module.exports = app;
