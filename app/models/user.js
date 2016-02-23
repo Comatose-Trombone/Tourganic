4d6c89ca38a0d8765aa3ef8528f0405b65a610c4
joshsheller <joshua.a.sheller@gmail.com>
2016-02-22 16:29:15 -0800

    Implement user.js / tour.js models and users.js / tours.js collections

---
+++ app/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+ username: String,
+ email: String,
+ description: String,
+ createdEvents: [],
+ attendingEvents: []
+});
+
+module.exports = mongoose.model('User', userSchema);
