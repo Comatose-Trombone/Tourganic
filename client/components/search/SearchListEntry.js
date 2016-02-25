92be15092a504a3b865db850a0439d54cd4282ed
Daniel Wong <danielwong2268@gmail.com>
2016-02-25 13:49:50 -0800

    Clicking on searchEntryTour now leads you to the tour page

--- client/components/search/SearchListEntry.js
+++ client/components/search/SearchListEntry.js
@@ -6,9 +6,13 @@ export default class SearchListEntry extends React.Component {
 		super(props)
 	}
 
+	handleTourClick() {
+    window.location = 'http://localhost:8080/#/profile/' + this.props.tour._id;
+  }
 	render() {
+
 		return (
-			<div className='searchTourEntry'>
+			<div className='searchTourEntry' onClick={this.handleTourClick.bind(this)}>
 				<div>{this.props.tour.name}</div>
 				<div>{this.props.tour.location}</div>
 				<div>price: ${this.props.tour.price}</div>
