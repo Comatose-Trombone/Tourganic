be0eeec61ebcb1e90723b3af6b14ad4f7c4cf066
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-02-27 14:16:19 -0800

    Place holders for createtour form

--- client/components/profile/CreateTourForm.js
+++ client/components/profile/CreateTourForm.js
@@ -5,12 +5,12 @@ export default class CreateTourForm extends React.Component {
     super(props);
     this.props = props;
     this.state = {
-      description: 'description',
-      name: 'name',
+      description: '',
+      name: '',
       createdBy: 'createdBy',
-      location: 'location',
-      price: 'price',
-      date: 'date'
+      location: '',
+      price: '',
+      date: ''
     }
 
   };
@@ -32,11 +32,11 @@ export default class CreateTourForm extends React.Component {
     return (
       <div>
         <form>
-          <input value={this.state.name} onChange={this.handleChange.bind(this, 'name')} onClick={this.reset.bind(this, 'name')}/><br/>
-          <input value={this.state.location} onChange={this.handleChange.bind(this, 'location')} onClick={this.reset.bind(this, 'location')}/><br/>
-          <input type='number' value={this.state.price} onChange={this.handleChange.bind(this, 'price')} onClick={this.reset.bind(this, 'price')}/><br/>
-          <input value={this.state.date} onChange={this.handleChange.bind(this, 'date')} onClick={this.reset.bind(this, 'date')}/><br/>
-          <textarea value={this.state.description} onChange={this.handleChange.bind(this, 'description')} onClick={this.reset.bind(this, 'description')}/><br/>
+          <input value={this.state.name} onChange={this.handleChange.bind(this, 'name')} onClick={this.reset.bind(this, 'name')} placeholder= "Enter the name of the tour"/><br/>
+          <input value={this.state.location} onChange={this.handleChange.bind(this, 'location')} onClick={this.reset.bind(this, 'location')} placeholder= "Enter the location of the tour"/><br/>
+          <input type='number' value={this.state.price} onChange={this.handleChange.bind(this, 'price')} onClick={this.reset.bind(this, 'price')} placeholder= "Enter the price of the tour"/><br/>
+          <input value={this.state.date} onChange={this.handleChange.bind(this, 'date')} onClick={this.reset.bind(this, 'date')} placeholder= "Enter the date of the tour"/><br/>
+          <textarea value={this.state.description} onChange={this.handleChange.bind(this, 'description')} onClick={this.reset.bind(this, 'description')} placeholder= "Brief description about the tour"/><br/>
           <input type='submit' value='Create Tour' onClick={this.props.submitNewTour.bind(this, this.state)}/>
         </form>
       </div>
