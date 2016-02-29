a4edb8eb4739416f4b7ad9563497ac1a363f6b7e
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-02-29 10:57:01 -0800

    cross origin implementation

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+// var headers = defaultCorsHeaders;
+// var headers['Content-Type'] = "text/plain"
+
+// response.writehead(statusCode, headers);
+
+// var defaultCorsHeaders = {
+//  "access-control-allow-origin": "https://tour-allure.herokuapp.com/",
+//  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+//  "access-control-allow-headers": "content-type, accept",
+//  "access-control-max-age": 10 // Seconds.
+// };
+
 app.use(express.static(__dirname + '/public'));
 
 require('./app/routes')(app);
